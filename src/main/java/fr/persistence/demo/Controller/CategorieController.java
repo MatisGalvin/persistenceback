880c3322a572a2f01850334773083cf2c5ea806f
Antonin SIMON <antonin.simon@epsi.fr>
2023-06-05 14:36:22 +0200

    manage deleting category

--- src/main/java/fr/persistence/demo/Controller/CategorieController.java
+++ src/main/java/fr/persistence/demo/Controller/CategorieController.java
@@ -18,6 +18,11 @@ public class CategorieController {
         return categorieRepository.findAll();
     }
 
+    @RequestMapping(path = "/categories/noflag", method = RequestMethod.GET)
+    public Iterable<Categorie> getAllNoFlagsCategories() {
+        return categorieRepository.getAllNoFlagsCategories();
+    }
+
     @RequestMapping(path = "/categorie", method = RequestMethod.POST)
     public Categorie addCategorie(@RequestBody Categorie categorie) {
         return categorieRepository.save(categorie);
@@ -34,13 +39,19 @@ public class CategorieController {
         return categorieRepository.save(categorie);
     }
 
-    @RequestMapping(path = "/categorie", method = RequestMethod.DELETE)
-    public Boolean deleteCategorieById(@RequestParam Long id){
-        Optional<Categorie> categorieToDelete = categorieRepository.findById(id);
-        if(categorieToDelete.isPresent()){
-            categorieRepository.deleteById(id);
-            return true;
-        }
-        return false;
+//    @RequestMapping(path = "/categorie", method = RequestMethod.DELETE)
+//    public Boolean deleteCategorieById(@RequestParam Long id){
+//        Optional<Categorie> categorieToDelete = categorieRepository.findById(id);
+//        if(categorieToDelete.isPresent()){
+//            categorieRepository.deleteById(id);
+//            return true;
+//        }
+//        return false;
+//    }
+
+    @RequestMapping(path = "/categorie/delete", method = RequestMethod.PATCH)
+    public Categorie updateFlagCategorieById(@RequestBody Categorie categorie) {
+        categorie.setFlag(true);
+        return categorieRepository.save(categorie);
     }
 }
