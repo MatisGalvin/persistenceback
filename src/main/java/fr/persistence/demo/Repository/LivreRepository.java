77fdd3e4e6489ab5c0d66976a6a35adb046e0b29
Antonin SIMON <antonin.simon@epsi.fr>
2023-06-05 16:01:09 +0200

    update

--- src/main/java/fr/persistence/demo/Repository/LivreRepository.java
+++ src/main/java/fr/persistence/demo/Repository/LivreRepository.java
@@ -1,7 +1,14 @@
 package fr.persistence.demo.Repository;
 
+import fr.persistence.demo.Entity.Categorie;
 import fr.persistence.demo.Entity.Livre;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface LivreRepository extends CrudRepository<Livre, Long> {
+
+    @Query("SELECT categorie FROM Categorie categorie WHERE categorie.flag = false")
+    List<Categorie> getAllNoFlagsCategories();
 }
